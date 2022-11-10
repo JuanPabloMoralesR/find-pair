5abcc67d60f7bd16591d917da79256d948e3e78e
Juan Pablo Morales <jupmoralesri@unal.edu.co>
2022-11-09 19:55:47 -0500

    Last commit: 7:55pm

--- index.php
+++ index.php
@@ -10,3 +10,6 @@
     print("<pre>".print_r((new PairFinder([2,5,1,3,4,6,7], 8))->find(),true)."</pre>");
     print("<pre>".print_r((new PairFinder([3,3,5,6,7] , 11))->find(),true)."</pre>");
     print("<pre>".print_r((new PairFinder([4,2,8,25], 26))->find(),true)."</pre>");
+
+
+    /* Made by: Juan Pablo Morales Rincón*/
