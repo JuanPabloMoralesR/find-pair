e01a70bb47cfb596a149601158c40b5eb163c96f
Juan Pablo Morales <jupmoralesri@unal.edu.co>
2022-11-09 19:30:38 -0500

    Added: require statements in files

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    require_once 'PairFinder.php';
+
     print("<pre>".print_r((new PairFinder([1,2,3,4], 5))->find(),true)."</pre>");
     print("<pre>".print_r((new PairFinder([2,5,1,3,4,6,7], 8))->find(),true)."</pre>");
     print("<pre>".print_r((new PairFinder([3,3,5,6,7] , 11))->find(),true)."</pre>");
