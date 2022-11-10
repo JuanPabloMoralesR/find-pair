d4be6706f7e1078317af106ff79268ac948f9fee
Juan Pablo Morales <jupmoralesri@unal.edu.co>
2022-11-09 19:27:38 -0500

    Added: test cases to index

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+    print("<pre>".print_r((new PairFinder([1,2,3,4], 5))->find(),true)."</pre>");
+    print("<pre>".print_r((new PairFinder([2,5,1,3,4,6,7], 8))->find(),true)."</pre>");
+    print("<pre>".print_r((new PairFinder([3,3,5,6,7] , 11))->find(),true)."</pre>");
+    print("<pre>".print_r((new PairFinder([4,2,8,25], 26))->find(),true)."</pre>");
