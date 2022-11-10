6e0f38e2b75a7d6247e4c66c6b3c84346e949ae8
Juan Pablo Morales <jupmoralesri@unal.edu.co>
2022-11-09 19:05:12 -0500

    Created: Initial configuration for PairFinder class

--- PairFinder.php
+++ PairFinder.php
@@ -3,4 +3,26 @@
     class PairFinder
     {
 
+        /**
+         * @var array
+         */
+        private array $entries;
+
+        /**
+         * @var int
+         */
+        private int $desiredSum;
+
+        /**
+         * @var \ArrayValidator
+         */
+        private ArrayValidator $arrayValidator;
+
+        public function __construct(array $entries, int $desiredSum)
+        {
+            $this->entries = $entries;
+            $this->desiredSum = $desiredSum;
+            $this->arrayValidator = new ArrayValidator($this->entries);
+        }
+
     }
